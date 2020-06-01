a84e40ade08112fc96d72f6b854abae73a38f283
ShadyFox2004 <amboudreau.amb@gmail.com>
2020-05-31 21:05:55 -0400

    Basic Logic added to the repository

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	// Pet state variables
+	int petName = "Ophelia";
+	int petMood = 1;
+	int petRate = 0;
+	int petSexe = 0;
+	//...
+	printf(petName);
+}	
